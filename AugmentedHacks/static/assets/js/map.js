86a17df34cd9118d11d4465279adf99cba0c3c1e
QuackingBob <quackingbob101@gmail.com>
2022-04-17 22:06:05 -0700

    fix zoom

--- AugmentedHacks/static/assets/js/map.js
+++ AugmentedHacks/static/assets/js/map.js
@@ -16,8 +16,8 @@ slider.oninput = function () {
 // Creating a map object
 var map = new L.map('map', mapOptions);
 var popmap = new L.map('pop-map', mapOptions);
-map.options.maxZoom = 14;
-popmap.options.maxZoom = 14;
+// map.options.maxZoom = 14;
+// popmap.options.maxZoom = 14;
 
 // Creating a Layer object
 var layer = new L.TileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=hM01WZAOFjwbYPSEZYOj', {
