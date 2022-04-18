70f85680a5e6568f967de8634909d04ab21ab2f1
alantian2018 <74695538+alantian2018@users.noreply.github.com>
2022-04-17 22:17:13 -0700

    climap

--- AugmentedHacks/static/assets/js/map.js
+++ AugmentedHacks/static/assets/js/map.js
@@ -6,11 +6,11 @@ var mapOptions = {
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML ='Year: '+ slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
-    output.innerHTML = this.value;
+    output.innerHTML ='Year: '+ this.value;
 }
 
 // Creating a map object
