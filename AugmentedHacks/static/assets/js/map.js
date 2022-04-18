11c8fa7b7f88a7470e314953c962e7899dba179b
Aakash Kumar <aakashinthesky@gmail.com>
2022-04-17 19:50:20 -0700

    a

--- AugmentedHacks/static/assets/js/map.js
+++ AugmentedHacks/static/assets/js/map.js
@@ -68,7 +68,7 @@ var poplayer = L.tileLayer.gl({
 //      }).addTo(popmap);
 //  });
 
-var geoJson = new L.GeoJSON.AJAX(static_url + "/countries.geo.json");
+var geoJson = new L.GeoJSON.AJAX(static_url + "assets/countries.geo.json");
 // console.log(geoJson);
 geoJson.addTo(popmap);
 
